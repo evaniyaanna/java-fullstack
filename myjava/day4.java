a219c4b6de1251a1f8b9df1f4f508513d0106158
evaniyaanna <evaniyaannaaa@gmail.com>
2026-08-17 16:55:15 +0530

    day4 done

---
+++ myjava/day4.java
@@ -0,0 +1,17 @@
+public class day4 {
+    public static void main(String[] args){
+        String defaultuser="admin";
+        String defpassword="java123";
+
+        String username="admin";
+        String password="java123";
+
+        if((defaultuser==username )&&(defpassword==password)){
+            System.out.println("login successful");
+        }
+            else
+                System.out.println("Access Denied");
+            
+        }
+    }
+
