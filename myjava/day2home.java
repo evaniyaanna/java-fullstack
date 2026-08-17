4df45d3f503d540f3a5c03bb1aedee165e6c35ca
evaniyaanna <evaniyaannaaa@gmail.com>
2026-08-17 15:17:06 +0530

    day2 homewrk done

--- myjava/day2home.java
+++ myjava/day2home.java
@@ -1,6 +1,11 @@
 public class day2home {
     public static void main(String[] args)
     {
-        
+        String ch="soap";
+        int qua=4;
+        double price=18.75d;
+        double total=qua*price;
+        System.out.println("item name="+ch+"\nitem quantity="+qua+"\nunit price="+price+"\ntotal price="+total);
+
     }
 }
