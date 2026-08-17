6280dfee6c8376f4ed5f7cf0ddc4cdfce9c418e3
evaniyaanna <evaniyaannaaa@gmail.com>
2026-08-17 15:07:28 +0530

    day2 done

---
+++ myjava/day2.java
@@ -0,0 +1,17 @@
+public class demo {
+
+	public static void main(String[] args) {
+		int noOfBooks=3;
+		int price=275;
+		double total=price*noOfBooks;
+		System.out.println("no of books="+noOfBooks+"\nprice per book="+price);
+		System.out.println(" total price="+total);
+		int discount=10;
+		double dis=discount;
+		double discounttotal=(dis/100)*total;
+		double finalamt=total-discounttotal;
+		System.out.println("final amount after discount="+finalamt);
+		
+	}
+
+}
