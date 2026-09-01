03dcaed3db2fd4362f4993ef721bdf3ec1c53108
evaniyaanna <evaniyaannaaa@gmail.com>
2026-09-01 14:18:58 +0530

    day11 done

--- myjava/CourseTest.java
+++ myjava/CourseTest.java
@@ -33,8 +33,10 @@ public class CourseTest {
     public static void main(String[] args) {
         course c1 = new course("Java Programming", 8, 30);
         course c2 = new course("Python Programming", 6, 25);
+        
         c1.displayDetails();
         c2.displayDetails();
+
         System.out.println("Total Students Enrolled: " + course.total);
         course.Platform.displayPlatformMessage();
     }
